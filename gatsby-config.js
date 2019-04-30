9129e68cba2f1186e4ac952e1830fe1c9beb6194
Amber Wilkie <amber@amberwilkie.com>
2019-04-30 08:27:41 +0200

    Include google analytics (#8)
    * Add google analytics
    * Pick up package-lock.json
    * I use yarn, not npm. I have lost my damn mind
    * Dammit gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,12 @@ module.exports = {
         // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-2837192-9",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
