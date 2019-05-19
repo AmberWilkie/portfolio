d9d91ccf6ec3f1e8ce915d427758835ca118c223
amberwilkie <amber@amberwilkie.com>
2019-05-19 22:00:22 +0200

    Add home button to articles

--- src/components/article-layout.js
+++ src/components/article-layout.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import '../assets/scss/main.scss'
 import Footer from '../components/Footer'
+import { Link } from 'gatsby'
 
 class ArticleTemplate extends React.Component {
   render() {
     return (
-      <div id='footer' className='article-body'>
-        <div className='article-inner'>
-          {this.props.children}
+      <div id='footer'>
+        <div className='back-link'><Link to='/'>{'<< Home'}</Link></div>
+        <div className='article-body'>
+          <div className='article-inner'>
+            {this.props.children}
+          </div>
+          <Footer/>
         </div>
-        <Footer/>
       </div>
     )
   }
